00e4f7a44015ce3757a28b8c2428c1cbafe38539
MurtazaBagwala07 <murtazabag786@gmail.com>
2021-07-20 23:09:50 +0530

    added files and coded

---
+++ script.js
@@ -0,0 +1,28 @@
+var translateButton = document.querySelector("#btn-Translate");
+var inputText = document.querySelector("#text-Area");
+var output = document.querySelector("#output");
+
+var serverUrl="";
+
+function translationUrl(text){
+    return serverUrl+"?"+"text="+text;
+}
+
+function errorHandler(error){
+    console.log("error occured",error);
+    alert("Something wrong with server try again later")
+}
+
+function clickHandler(){
+    
+    var inputTxt = inputText.value;
+    fetch(getTranslationURL(inputTxt))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        output.innerHTML = translatedText;
+    })
+    .catch(errorHandler)
+};
+
+translateButton.addEventListener("click", clickHandler)
